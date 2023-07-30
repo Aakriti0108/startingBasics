8c6f4103e37521e9b0d1a7a637d0e2b8fa1ebb59
Aakriti yadav <aakritiyadav0108@gmail.com>
2023-07-30 23:13:18 +0530

    props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+   const expense =[
+    {
+      date:new Date("2022-03-25"),
+      Tit:'sweet',
+      amount:94.12,
+      spent:85
+    },
+    {
+      date:new Date(2020,7,14),
+      Tit:'cheese',
+      amount:64.12,
+      spent:58
+    }
+   ]
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem 
+      date={expense[0].date}
+      Tit={expense[0].Tit}
+      amount={expense[0].amount}
+      spent={expense[0].spent}>
+      </ExpenseItem>
+      <ExpenseItem
+      date={expense[1].date}
+      Tit={expense[1].Tit}
+      amount={expense[1].amount}
+      spent={expense[1].spent}>
+      </ExpenseItem>
+      
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,19 +1,17 @@
 import './ExpenseItem.css';
-function ExpenseItem(){
+function ExpenseItem(props){
 
-    const current = new Date();
-    const expenseTitle = 'Car Insurances';
-    const expenseprice = 246.57;
-    const LocationOfExpenditure = 200;
+  
     return (
         <div>
         <div className='expense-item'>
-        <div>{current.getDate()}/{current.getMonth()+1}/{current.getFullYear()}</div>
+        <div>{props.date.toDateString()}</div>
         <div className='expense-item__description'>
-        <h2>{expenseTitle}</h2>
+        <h2>{props.Tit}</h2>
         </div>
-        <div className='expense-item__price'>${expenseprice}</div>
-        <div className='expense-item__price'>${LocationOfExpenditure}</div>
+        <div className='expense-item__price'>${props.amount}</div>
+        
+        <div className='expense-item__price'>${props.spent}</div>
         </div>
         </div>
         
