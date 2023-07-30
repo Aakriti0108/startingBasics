8e7671fb4b7f20d1c548e8fae3af665264cd6f6c
Aakriti yadav <aakritiyadav0108@gmail.com>
2023-07-30 17:41:52 +0530

    componnets

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,20 @@
+import './ExpenseItem.css';
 function ExpenseItem(){
+
+    const current = new Date();
+    const expenseTitle = 'Car Insurances';
+    const expenseprice = 246.57;
+    const LocationOfExpenditure = 200;
     return (
         <div>
-        <h2> Expense Item !!!</h2>
-        <p> Food Rs 10 </p>
-        <p> Petrol Rs 100 </p>
-        <p> Movies Rs 200</p>
+        <div className='expense-item'>
+        <div>{current.getDate()}/{current.getMonth()+1}/{current.getFullYear()}</div>
+        <div className='expense-item__description'>
+        <h2>{expenseTitle}</h2>
+        </div>
+        <div className='expense-item__price'>${expenseprice}</div>
+        <div className='expense-item__price'>${LocationOfExpenditure}</div>
+        </div>
         </div>
         
     );
