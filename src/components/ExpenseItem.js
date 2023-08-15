39679bf0f2870ff19426fd7158aade713b66c9cd
Aakriti yadav <aakritiyadav0108@gmail.com>
2023-08-15 12:50:41 +0530

    advance react 2

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,6 +1,7 @@
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from './Card';
+import { useState } from 'react';
 // function ExpenseItem(props){
 //   
 //     return (
@@ -20,17 +21,23 @@ import Card from './Card';
 // }
 
  const ExpenseItem = (props)=>{
+    const [Tit,setTit] = useState(props.Tit);
+
+    const [amount,setAmount] = useState(props.amount);
     const clickHandler = () =>{
-        console.log("Clicked !!!!");
+        setTit("WELCOME");
+    }
+    const increasement =()=>{
+        setAmount( amount + 100);
     }
     return (
         <div>
         <div className='expense-item'>
         <ExpenseDate date={props.date}/>
         <div className='expense-item__description'>
-        <h2>{props.Tit}</h2>
+        <h2>{Tit}</h2>
         </div>
-        <div className='expense-item__price'>${props.amount}</div>
+        <div className='expense-item__price'><button onClick={increasement}>${amount}</button></div>
         
         <div className='expense-item__price'>${props.spent}</div>
         <button onClick ={clickHandler}>Change  Title</button>
